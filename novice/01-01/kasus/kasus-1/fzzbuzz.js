a0981ad273d6e26b3898e3ab6499f24a7f0e6d1c
Jembar <jembar.ashofa01@gmail.com>
2020-08-18 14:19:57 +0700

    tambah fzzbuzz

---
+++ novice/01-01/kasus/kasus-1/fzzbuzz.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    if (i % 3 === 0) {
+        console.log('Fizz')
+        if (i % 5 === 0 && i % 5 !== 3) {
+            console.log('Buzz')
+            if (i % 5 === 0 && i % 3 === 0){
+                console.log('FizzBuzz')
+            }
+        }
+    }
+    
+}
