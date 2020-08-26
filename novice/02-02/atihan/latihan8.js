5123804fb835190600b033fa4a0ff7c376c8fe62
Jembar <jembar.ashofa01@gmail.com>
2020-08-26 11:41:38 +0700

    update latihan 8

--- novice/02-02/atihan/latihan8.js
+++ novice/02-02/atihan/latihan8.js
@@ -3,4 +3,22 @@ class Hero {
         this.name = name;
         this.level = level;
     }
+    greet() {
+        return `${this.name} adalah Nama saya`;
+    }
 }
+
+class Mage extends Hero {
+    constructor(name, level, spell) {
+        super(name, level);
+
+        // propeti baru
+        this.spell = spell;
+    }
+    serang() {
+        console.log(`Rassaaaakaannn iniiiiiii musuh-kuuuunn, ${this.spell}`);
+    }
+}
+
+let momochan = new Mage('momochan', 3, 'Fayeru Bolt');
+momochan.serang();
