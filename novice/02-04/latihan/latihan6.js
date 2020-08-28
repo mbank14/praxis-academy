c6afbe40259f825d2655f5880534e421daa7b023
Jembar <jembar.ashofa01@gmail.com>
2020-08-28 11:10:20 +0700

    update latihan 6

---
+++ novice/02-04/latihan/latihan6.js
@@ -0,0 +1,23 @@
+//contoh simulasi callback untuk asynchronous
+
+function satu(){
+	console.log("fungsi pertama");
+}
+
+function dua(){
+    //set delay
+  setTimeout(
+    function(){
+         console.log("fungsi kedua");    
+   }, 
+   //nilai delay
+   100);
+}
+
+function tiga(){
+	console.log("fungsi ketiga");
+}
+
+satu();
+dua();
+tiga();
