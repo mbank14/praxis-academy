046e9c69ca92ef5b1128e3cbe7d37ef518fc679b
Jembar <jembar.ashofa01@gmail.com>
2020-08-31 13:49:18 +0700

    update kuis try...catch

---
+++ novice/02-05/kasus/kasus1.js
@@ -0,0 +1,21 @@
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+  // Your code here.
+  try{
+    console.log("aw aw ", Number(primitiveMultiply(a,b)));
+  }catch(e){
+	console.log("ada yang error ",e);
+}
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
