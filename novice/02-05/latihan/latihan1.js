473ce0354a02127af871b94b6e35e11e74e469a4
Jembar <jembar.ashofa01@gmail.com>
2020-08-31 12:08:31 +0700

    add files

---
+++ novice/02-05/latihan/latihan1.js
@@ -0,0 +1,17 @@
+function coba() {
+    "use strict";//fungsi ini berfungsi jika ada variabel yang tidak di definisikan aan eror
+    
+    //contoh error
+    for (hitung = 0; hitung < 10; hitung++) {
+      console.log("hitung dsfn");
+    }
+
+    //contoh tidak error
+    // for (hitung = 0; hitung < 10; hitung++) {
+    //     console.log("hitung dsfn");
+    //   }
+  }
+  
+coba();
+
+
---
+++ novice/02-05/latihan/latihan2.js
@@ -0,0 +1,7 @@
+//  STRICT MODE 
+'use strict' // jika ini tidak digunakan maka output akan menghasilkan "lukamu"
+function Hooman(name){
+    this.name = name;
+}
+let manusia = Hooman("lukamu");
+console.log(name);
