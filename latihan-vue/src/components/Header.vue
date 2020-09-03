6c00c7222add6e9f771670b06771b23add20a860
Jembar <jembar.ashofa01@gmail.com>
2020-09-03 16:55:57 +0700

    latiha

---
+++ latihan-vue/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+       <section>
+           <h1>ini latihan kasus</h1>
+           <data-dummy></data-dummy>
+        </section> 
+    </div>
+</template>
+<script>
+import DataDummy from './child/DataDummy.vue';
+
+export default {
+    name: 'Header',
+    components: {
+        DataDummy,
+    },
+    data: function () {
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
---
+++ latihan-vue/src/components/child/DataDummy.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h3>Ini untuk data dummy</h3>
+        <table >
+            <thead>
+            <tr>
+                <th>TITLE</th>
+                <th>KONTEN</th>
+               
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="data in dataku" :key="data">
+                <td>{{ data.id }}</td>
+                <td>{{ data.body}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DataDummy',
+    data: function(){
+        return{
+            dataku: [],
+        }
+    },
+    beforeMount(){
+    this.getDataDummy();
+  },
+    methods: {
+        async getDataDummy(){
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await res.json();
+            this.dataku = data;
+        },
+    },
+}
+</script>
+<style  scoped>
+
+</style>
